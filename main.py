8ad72d2aaedda05863e3b065eac815a0abd89208
kmdavidds <kmdavidds@gmail.com>
2024-11-03 11:37:30 +0700

    chore: remove code comments

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0")
 
-    # Load a model
-    model = YOLO("yolov8n.pt").to(device=device) # load a pretrained model (recommended for training)
+    model = YOLO("yolov8n.pt").to(device=device)
 
-    # Use the model
-    model.train(data="./data.yaml", epochs=10, amp=False)  # train the model
+    model.train(data="./data.yaml", epochs=10, amp=False) 
     model.val()
